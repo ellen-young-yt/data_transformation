985e64e3381f93ad5eb48205f168fd0250f8a85e
Mike Adamski <m.adamski3@gmail.com>
2025-10-19 15:18:56 -0500

    Update video_history mart to utilize the summarized version of channel_combined

--- models/marts/video_history.sql
+++ models/marts/video_history.sql
@@ -3,9 +3,9 @@ with videos as (
     from {{ ref('int_video') }}
 ),
 
-channel_combined as (
+views_and_watch_time as (
     select *
-    from {{ ref('int_channel_combined__video' ) }}
+    from {{ ref('int_channel_combined_summarized_by_video_and_day' ) }}
 ),
 
 channel_basic as (
@@ -26,64 +26,6 @@ audience_retention as (
 comments as (
     select *
     from {{ ref('stg_youtube_analytics__comment') }}
-),
-
-views_and_watch_time as (
-    select
-        video_id,
-        calendar_date,
-
-        /* VIEW COUNTS */
-
-        sum(view_count) as total_views,
-        -- by subscriber status
-        sum(iff(subscribed_status = 'Subscribed', view_count, 0)) as subscriber_views,
-        sum(iff(subscribed_status = 'Not Subscribed', view_count, 0)) as non_subscriber_views,
-        -- by country
-        sum(iff(country_name = 'United States of America', view_count, 0)) as united_states_views,
-        sum(iff(country_name = 'Korea, Republic of', view_count, 0)) as korea_views,
-        sum(
-            iff(
-                country_name not in ('United States of America', 'Korea, Republic of'),
-                view_count,
-                0
-            )
-        ) as other_country_views,
-        -- by device type
-        sum(iff(device_type = 'Mobile phone', view_count, 0)) as mobile_phone_device_views,
-        sum(iff(device_type = 'TV', view_count, 0)) as television_device_views,
-        sum(iff(device_type not in ('Mobile phone', 'TV'), view_count, 0)) as other_device_views,
-
-        /* WATCH TIME */
-
-        sum(watch_time_in_minutes) as total_watch_time_in_mins,
-        -- by subscriber status
-        sum(iff(subscribed_status = 'Subscribed', watch_time_in_minutes, 0))
-            as subscriber_watch_time_in_mins,
-        sum(iff(subscribed_status = 'Not Subscribed', watch_time_in_minutes, 0))
-            as non_subscriber_watch_time_in_mins,
-        -- by country
-        sum(iff(country_name = 'United States of America', watch_time_in_minutes, 0))
-            as united_states_watch_time_in_mins,
-        sum(iff(country_name = 'Korea, Republic of', watch_time_in_minutes, 0))
-            as korea_watch_time_in_mins,
-        sum(
-            iff(
-                country_name not in ('United States of America', 'Korea, Republic of'),
-                watch_time_in_minutes,
-                0
-            )
-        ) as other_country_watch_time_in_mins,
-        -- by device type
-        sum(iff(device_type = 'Mobile phone', watch_time_in_minutes, 0))
-            as mobile_phone_device_watch_time_in_mins,
-        sum(iff(device_type = 'TV', watch_time_in_minutes, 0))
-            as television_device_watch_time_in_mins,
-        sum(iff(device_type not in ('Mobile phone', 'TV'), watch_time_in_minutes, 0))
-            as other_device_watch_time_in_mins
-
-    from channel_combined
-    group by all
 ),
 
 subscribers_likes_and_dislikes as (
